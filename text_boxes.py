c0627f90c6503f8a0fd53dbbdf088681d18f626d
Sam Loeffler <samloeffler123@gmail.com>
2026-02-02 06:18:53 -0600

    added border frame to instance and dragging of text_box

---
+++ text_boxes.py
@@ -0,0 +1,49 @@
+import customtkinter
+import tkinter
+from constants import BORDER_WIDTH, CORNER_RADIUS
+
+class TextBoxes(customtkinter.CTkFrame):
+    def __init__(self, parent, app):
+        super().__init__(parent)
+
+        self.app = app
+
+
+        self.board_frame = customtkinter.CTkFrame(self, border_width=BORDER_WIDTH, corner_radius=CORNER_RADIUS)
+        self.board_frame.pack()
+
+        self.textbox = customtkinter.CTkTextbox(self.board_frame, border_width=0)
+        self.textbox.pack()
+
+        self.root = self.winfo_toplevel()
+
+        self.textbox.bind("<FocusIn>", self.on_focus)
+        self.textbox.bind("<B1-Motion>", self.on_drag)
+        self.textbox.bind("<Button-1>", self.on_left_click)
+        self.textbox.bind("<ButtonRelease>", self.on_drop)
+
+    def get_highlighted_text(self):
+        try:
+            start = self.textbox.index("sel.first")
+            end = self.textbox.index("sel.last")
+            self.textbox.tag_add("sel_txt", start, end)
+            self.textbox.tag_config("sel_txt", background="yellow", foreground="red")
+        except tkinter.TclError:
+            pass 
+    
+    def on_left_click(self, event):
+        
+        self.board_frame
+
+        self.dragging = True
+
+    def on_focus(self, event):
+        self.app.active_textbox = self
+    
+    def on_drag(self, event):
+        if self.dragging:
+            self.place(x=self.root.winfo_pointerx()-self.root.winfo_rootx(), y=self.root.winfo_pointery()-self.root.winfo_rooty())
+
+    def on_drop(self, event):
+        if self.dragging:
+            self.place(x=self.root.winfo_pointerx()-self.root.winfo_rootx(), y=self.root.winfo_pointery()-self.root.winfo_rooty())
