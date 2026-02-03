8ad1ffed6618b714f1bde6ad9544070204644afd
Sam Loeffler <samloeffler123@gmail.com>
2026-02-03 07:12:25 -0600

    removed border frame

--- text_boxes.py
+++ text_boxes.py
@@ -1,26 +1,12 @@
 import customtkinter
 import tkinter
-from constants import BORDER_WIDTH, CORNER_RADIUS
 
 class TextBoxes(customtkinter.CTkFrame):
-    def __init__(self, parent, app):
+    def __init__(self, parent):
         super().__init__(parent)
 
-        self.app = app
-
-
-        self.board_frame = customtkinter.CTkFrame(self, border_width=BORDER_WIDTH, corner_radius=CORNER_RADIUS)
-        self.board_frame.pack(fill="both", expand=True)
-        self.board_frame.configure(border_color="gray")
-
-        self.textbox = customtkinter.CTkTextbox(self.board_frame, width=150, height=150)
-        self.textbox.pack(fill="both", expand=True, padx=BORDER_WIDTH, pady=BORDER_WIDTH)
-
-        self.root = self.winfo_toplevel()
-
-        self.board_frame.bind("<FocusIn>", self.on_focus)
-        self.board_frame.bind("<B1-Motion>", self.on_drag)
-        self.board_frame.bind("<ButtonRelease>", self.on_drop)
+        self.textbox = customtkinter.CTkTextbox(self, width=150, height=150)
+        self.textbox.pack(fill="both", expand=True)
 
     def get_highlighted_text(self):
         try:
@@ -31,11 +17,4 @@ class TextBoxes(customtkinter.CTkFrame):
         except tkinter.TclError:
             pass 
 
-    def on_focus(self, event):
-        self.app.active_textbox = self
     
-    def on_drag(self, event):
-        self.place(x=self.root.winfo_pointerx()-self.root.winfo_rootx(), y=self.root.winfo_pointery()-self.root.winfo_rooty())
-
-    def on_drop(self, event):
-        self.place(x=self.root.winfo_pointerx()-self.root.winfo_rootx(), y=self.root.winfo_pointery()-self.root.winfo_rooty())
