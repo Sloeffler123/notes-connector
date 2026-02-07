45ac9397f9d2a0dcdaec942b1628b33f2918115c
Sam Loeffler <samloeffler123@gmail.com>
2026-02-07 16:29:51 -0600

    frame is resizable

--- border_frame.py
+++ border_frame.py
@@ -2,12 +2,14 @@
 import customtkinter
 from constants import BORDER_WIDTH, CORNER_RADIUS
 
+
+
 class BorderFrame(customtkinter.CTkFrame):
     def __init__(self, parent, app):
-        super().__init__(parent, border_width=BORDER_WIDTH, corner_radius=CORNER_RADIUS)
+        super().__init__(parent, border_width=BORDER_WIDTH, cursor="fleur")
 
         self.app = app
-
+        
         self.frame = customtkinter.CTkFrame(self)
         self.frame.pack(fill="both", expand=True, padx=BORDER_WIDTH, pady=BORDER_WIDTH)
 
@@ -16,16 +18,41 @@ class BorderFrame(customtkinter.CTkFrame):
         
         self.bind("<Button-1>", self.on_click)
         self.bind("<B1-Motion>", self.on_drag)
+        self.bind("<ButtonRelease-1>", self.on_realease)
+        self.bind("<Enter>", self.on_enter)
         self.frame.bind("<Button-1>", self.on_click)
         self.frame.bind("<B1-Motion>", self.on_drag)
-    
-    
+
     def on_click(self, event):
         self.app.active_frame = self
-        self.x_drag = event.x
-        self.y_drag = event.y
+        
+        width = self.winfo_width()
+        height = self.winfo_height()
 
+        if (event.x > width - 10) and (event.y > height - 10):
+            print("in corner")
+            self.not_in_corner = False
+        else:
+            self.x_drag = event.x
+            self.y_drag = event.y
+            self.not_in_corner = True
+            self.width_start = event.x
+            self.height_start = event.y
+            
+    def on_enter(self, event):
+        width = self.winfo_width()
+        height = self.winfo_height()
+        if event.x > width - 10 and event.y > height - 10:
+            self.configure(cursor="bottom_right_corner")
+            
     def on_drag(self, event):
-        x = self.winfo_x() + event.x - self.x_drag
-        y = self.winfo_y() + event.y - self.y_drag
-        self.place(x=x, y=y)
+        if self.not_in_corner:
+            x = self.winfo_x() + event.x - self.x_drag
+            y = self.winfo_y() + event.y - self.y_drag
+            self.place(x=x, y=y)
+        else:
+            self.configure(cursor="bottom_right_corner")
+            self.place_configure(width=event.x, height=event.y)
+    
+    def on_realease(self, event):
+        self.configure(cursor="fleur")
